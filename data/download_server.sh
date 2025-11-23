d9905e091e5dc20ed677bbf74e884bc0b6409541
Mechetel <dima.homa5@gmail.com>
2025-11-24 01:06:26 +0200

    feat: add boss_256_0.4 on server

--- data/download_server.sh
+++ data/download_server.sh
@@ -1,13 +1,24 @@
 apt install python3-pip unzip -y
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
-pip3 install imageio tqdm reedsolo
+pip3 install imageio tqdm reedsolo scikit-learn
+
+
 
 mkdir -p ~/data
 curl -L -o ~/data/GBRASNET.zip https://www.kaggle.com/api/v1/datasets/download/zapak1010/bossbase-bows2
 unzip ~/data/GBRASNET.zip -d ~/data/
 rm ~/data/GBRASNET.zip
-
 cp -r ~/data/GBRASNET/BOSSbase-1.01 ~/data/GBRASNET/BOSSbase-1.01-div
+
+# Alternative download of BOSSbase from another source
+mkdir -p ~/data
+curl -L -o ~/data/bossbase.zip https://www.kaggle.com/api/v1/datasets/download/lijiyu/bossbase
+unzip ~/data/bossbase.zip -d ~/data/
+rm ~/data/bossbase.zip
+
+
 
 git clone https://github.com/Mechetel/SRNet.git
 bash ~/SRNet/data/split_server.sh
+# or
+bash ~/SRNet/data/split_server_bossbase.sh
